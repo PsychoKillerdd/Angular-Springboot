afac785f7b8232cd95317a75fae640ab1f767513
PsychoKillerdd <onlyvicenteuribe@gmail.com>
2026-04-08 21:43:52 -0400

    conditionals in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+  //esto es una clase como tal su nobre es appComponent se exporta como guard
 export class AppComponent {
-  title = 'angular';
+  public title:string = '¡Hola mundo! En angular';
+  public name:string = 'Angular';
+  public users:string[] = [];
+  public items:string[] = ['Item 1', 'Item 2', 'Item 3'];
+
+  public greet():void {
+    console.log('Hola mundo');
+  }
 }
