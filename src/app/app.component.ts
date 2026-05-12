82c0f103223880a2f661fd29699c58ea67505276
PsychoKillerdd <onlyvicenteuribe@gmail.com>
2026-05-12 18:22:20 -0400

    12/05

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,7 +14,27 @@ export class AppComponent {
   public title:string = '¡Hola mundo! En angular';
   public name:string = 'Angular';
   public users:string[] = [];
-  public items:string[] = ['Item 1', 'Item 2', 'Item 3'];
+  public items:string[] = ['Item 1', 'Item 2', 'Item 3','Item 4'];
+
+  public visible: boolean = false;
+
+  public ternario:boolean = true;
+
+  public buttonText:boolean = true;
+
+  setButtonText = (): void => {
+    this.buttonText = !this.buttonText;
+  };
+
+  setTernario(): void {
+    this.ternario = !this.ternario;
+    console.log("Hiciste click en el boton de cambio de persona");
+  };
+
+  setVisibility(): void {
+    this.visible = !this.visible;
+    this.buttonText = !this.buttonText;
+  }
 
   public greet():void {
     console.log('Hola mundo');
